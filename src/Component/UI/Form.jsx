d2a6e34e0fec78ddfb1e9511e380521ad8f57ea3
Mateeb Alharbi <63017846+Mateeb11@users.noreply.github.com>
2023-05-15 23:02:36 +0300

    fixing the gap between the title and the description in form component

--- src/Component/UI/Form.jsx
+++ src/Component/UI/Form.jsx
@@ -3,8 +3,8 @@ import classes from "./Form.module.scss";
 export default function Form({ title, description, className, children }) {
   return (
     <div className={`${classes.form} ${className}`}>
-      <h1>{title}</h1>
-      <p>{description}</p>
+      <h1 className={classes.title}>{title}</h1>
+      <p className={classes.description}>{description}</p>
       {children}
     </div>
   );
